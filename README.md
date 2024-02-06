ef8bb19e2509d79b443a8e9c361713805c668d4d
José Antonio Muñoz Jiménez <jamj2000@gmail.com>
2024-02-06 19:02:58 +0100

    Init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Solución a Prueba DWES Tema 5, 6 y 7
+
+[Enunciado de la prueba](Enunciado_Prueba.pdf)
---
+++ src/components/ListaZoos.js
@@ -0,0 +1,33 @@
+import { getAnimal, getZoos } from '@/lib/actions'
+
+async function ListaZoos({ animalId, disabled }) {
+    const zoos = await getZoos()
+
+    let animal = null;
+    let zooId = null;
+    if (animalId) {
+        animal = await getAnimal(animalId)
+        zooId = animal.zooId;
+    }
+
+    console.log(`ZOOLÓGICO del ANIMAL ${animalId}: `, zooId);
+
+    return (
+        <fieldset disabled={disabled}>
+            <legend>Zoos</legend>
+            {zoos?.map((zoo) => (
+                <div key={zoo.id}>
+                    <p>
+                        {zooId == zoo.id
+                            ? <input type='radio' name='zooId' value={zoo.id} defaultChecked />
+                            : <input type='radio' name='zooId' value={zoo.id} />
+                        }
+                        {zoo.nombre}
+                    </p>
+                </div>
+            ))}
+        </fieldset>
+    )
+}
+
+export default ListaZoos
