994cb51d45c1cd328b6b389cba6f980a997ff8e4
José A. Muñoz Jiménez <jamj2000@gmail.com>
2024-06-12 22:38:33 +0200

    select without disabled

--- src/components/ListaZoos.js
+++ src/components/ListaZoos.js
@@ -12,22 +12,46 @@ async function ListaZoos({ animalId, disabled }) {
 
     console.log(`ZOOLÓGICO del ANIMAL ${animalId}: `, zooId);
 
+    //// Tenemos 2 alternativas:
+    //// - usar inputs tipo radio
+    //// - usar options dentro de select
+
+
     return (
         <fieldset disabled={disabled}>
             <legend>Zoos</legend>
             {zoos?.map((zoo) => (
                 <div key={zoo.id}>
-                    <p>
-                        {zooId == zoo.id
-                            ? <input type='radio' name='zooId' value={zoo.id} defaultChecked />
-                            : <input type='radio' name='zooId' value={zoo.id} />
-                        }
-                        {zoo.nombre}
-                    </p>
+                    {zooId == zoo.id
+                        ? <input type='radio' name='zooId' value={zoo.id} defaultChecked />
+                        : <input type='radio' name='zooId' value={zoo.id} />
+                    }
+                    {zoo.nombre}
                 </div>
             ))}
         </fieldset>
-    )
+        )
+    
+        //// Si quieres utilizar options dentro de un select, comenta las líneas anteriores y 
+        //// y descomenta las siguientes líneas 
+        //// IMPORTANTE:  No poner disabled={disabled} en select puesto que no devuelve el valor correctamente
+    
+        // if (disabled)
+        //     return (
+        //         <fieldset>
+        //             <input type='hidden' name="zooId" value={zooId} />
+        //             <legend>Zoo</legend>
+        //             <strong>{zoos.find(p => p.id == zooId).nombre}</strong>
+        //         </fieldset>
+        //     )
+        // else
+        //     return (
+        //         <select name="zooId" defaultValue={zooId} >
+        //             {zoos?.map((zoo) => <option key={zoo.id} value={zoo.id}> {zoo.nombre} </option>
+        //             )}
+        //         </select>
+        //     )
+    
 }
 
 export default ListaZoos
